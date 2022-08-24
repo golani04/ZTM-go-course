ca0f7693ed5f541c704c367da3ceeb9306d184dc
Leonid <golani04@gmail.com>
2022-08-24 13:18:08 +0300

    learning ranges

--- lectures/demo/ranges/ranges.go
+++ lectures/demo/ranges/ranges.go
@@ -3,4 +3,15 @@ package main
 import "fmt"
 
 func main() {
+	slice := make([]string, 3)
+	slice = []string{"Hello", "World", "!"}
+
+	for i, elem := range slice {
+		fmt.Print(i, ") ", elem, ":")
+
+		for _, ch := range elem {
+			fmt.Printf(" %q", ch)
+		}
+		fmt.Println()
+	}
 }
