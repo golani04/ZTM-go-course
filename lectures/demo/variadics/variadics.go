eefc59b20566998e22fd71ff5fed8a83096cc8fe
Leonid <golani04@gmail.com>
2022-08-31 17:10:08 +0300

    learning of variadic functions

--- lectures/demo/variadics/variadics.go
+++ lectures/demo/variadics/variadics.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+func sum(nums ...int) int {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+
+	return sum
+}
+
 func main() {
+	a := []int{1, 2, 3}
+	b := []int{4, 6, 8}
+
+	all := append(a, b...)
+	fmt.Println(sum(all...))
+
+	answer := sum(1, 2, 3, 4, 5, 6, 7)
+	fmt.Println(answer)
 
 }
