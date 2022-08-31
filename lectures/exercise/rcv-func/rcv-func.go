75183a5e517aef7831004efbf524f5754b082c7c
Leonid <golani04@gmail.com>
2022-08-31 16:35:22 +0300

    learning receiver functions, kind of methods in classes in other languages

--- lectures/exercise/rcv-func/rcv-func.go
+++ lectures/exercise/rcv-func/rcv-func.go
@@ -16,6 +16,69 @@ package main
 
 import "fmt"
 
-func main() {
+type Name string
+type Stat uint
 
+type Player struct {
+	name      Name
+	health    Stat
+	maxHealth Stat
+	energy    Stat
+	maxEnergy Stat
+}
+
+func (player *Player) addHealth(amount Stat) {
+	result := player.health + amount
+	if player.maxHealth < result {
+		player.health = player.maxHealth
+	} else {
+		player.health = result
+	}
+	fmt.Println(player.name, "received", amount, "points to health. Health:", player.health)
+}
+
+func (player *Player) applyDamage(amount Stat) {
+	result := int(player.health - amount)
+	if result < 0 {
+		player.health = 0
+	} else {
+		player.health = Stat(result)
+	}
+	fmt.Println(player.name, "received damaged of", amount, "points. Health:", player.health)
+}
+
+func (player *Player) addEnergy(amount Stat) {
+	result := player.energy + amount
+	if player.maxEnergy < result {
+		player.energy = player.maxEnergy
+	} else {
+		player.energy = result
+	}
+	fmt.Println(player.name, "received", amount, "points to energy. Energy:", player.energy)
+}
+
+func (player *Player) consumeEnergy(amount Stat) {
+	result := int(player.energy - amount)
+	fmt.Println(result, player.energy)
+	if result < 0 {
+		player.energy = 0
+	} else {
+		player.energy = Stat(result)
+	}
+	fmt.Println(player.name, "consumed", amount, "points of the energy. Energy:", player.energy)
+}
+
+func main() {
+	player := Player{"Steve", 100, 100, 200, 200}
+	player.applyDamage(90)
+	player.addHealth(11)
+	player.consumeEnergy(50)
+	player.addEnergy(23)
+
+	// extremes
+	player.consumeEnergy(10000)
+	player.addEnergy(100000)
+
+	player.applyDamage(101)
+	player.addHealth(10001)
 }
